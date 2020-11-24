c93cd2eaa587a6f9ef2ffe84f44d71b7d5779a2f
Fatih Can <ybsfatihcan@gmail.com>
2020-11-25 00:13:54 +0300

    model added

--- app/src/main/java/com/example/todolistapp/model/Model.kt
+++ app/src/main/java/com/example/todolistapp/model/Model.kt
@@ -1,4 +1,11 @@
 package com.example.todolistapp.model
 
 class Model {
+    companion object  Factory{
+        fun createList(): Model = Model()
+    }
+
+    var UID: String? = null
+    var itemsDataText: String? = null
+    var doneOkay: Boolean? = false
 }
