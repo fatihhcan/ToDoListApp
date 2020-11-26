5fa9e4501789fd3e6fcc5ab7690cd30037a3cd60
Fatih Can <ybsfatihcan@gmail.com>
2020-11-26 13:32:47 +0300

    alert dialog added

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.todolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.todolistapp.model.Model
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<View>(R.id.addButton) as FloatingActionButton
+
+        database = FirebaseDatabase.getInstance().reference
+
+        addButton.setOnClickListener {  view ->
+            val alertDialog = AlertDialog.Builder(this)
+            val editText = EditText(this)
+            alertDialog.setMessage("Yapılacaklar Listesine Ekle")
+            alertDialog.setTitle("Başlık")
+            alertDialog.setView(editText)
+            alertDialog.setPositiveButton("Ekle"){dialog, i ->
+                val todoItemData = Model.createList()
+                todoItemData.itemsDataText = editText.text.toString()
+                todoItemData.doneOkay = false
+
+                val newItemData = database.child("todo").push()
+                todoItemData.UID=newItemData.key
+
+                newItemData.setValue(todoItemData)
+
+                dialog.dismiss()
+                Toast.makeText(this,"oluşturuldu", Toast.LENGTH_LONG).show()
+            }
+            alertDialog.show()
+        }
     }
 }
